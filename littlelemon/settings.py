d36891cabb01353bd00ecade49327bb77ce7714d
HajirEltayb <hajireltayb135@gmail.com>
2025-06-25 13:15:25 +0200

    commiting

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # The settings for app updated for the Graded assessment
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
     'restaurant',
 ]
 
@@ -135,3 +138,34 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK ={
+    'DEFAULT_RENDERER_CLASSES':[
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        #'rest_framework_xml.renderers.XMLRenderer',
+    ],"""
+    'DEFAULT_FILTER_BACKENDS':[
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
+    ],"""
+    'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE':2,
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+       #'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+   #'DEFAULT_THROTTLE_RATES':{
+      # 'anon':'2/minute',
+       #'user':'5/minute',
+      # 'ten':'10/minute',
+   #}
+
+}
+
+DJOSER={
+    "USER_ID_FIELD":"username"
+    
+}
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,14 +1,18 @@
 # from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
 from .forms import BookingForm
 from .models import Menu
-from django.core import serializers
+from django.core.paginator import Paginator,EmptyPage
 from .models import Booking
 from datetime import datetime
 import json
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
-
+from rest_framework.decorators import api_view
+from .serializers import MenuItemSerializer,BookingSerializer
+from rest_framework import status
+from rest_framework.response import Response
 
 # Create your views here.
 def home(request):
@@ -20,7 +24,7 @@ def about(request):
 def reservations(request):
     date = request.GET.get('date',datetime.today().date())
     bookings = Booking.objects.all()
-    booking_json = serializers.serialize('json', bookings)
+    booking_json = BookingSerializer(bookings)
     return render(request, 'bookings.html',{"bookings":booking_json})
 
 def book(request):
@@ -33,20 +37,77 @@ def book(request):
     return render(request, 'book.html', context)
 
 
-# Add your code here to create new views
+@api_view(['GET','POST','PUT','DELETE','PATCH'])
 def menu(request):
-    menu_data = Menu.objects.all()
-    main_data = {"menu": menu_data}
-    return render(request, 'menu.html', {"menu": main_data})
+    if request.method == 'GET':
+        items = Menu.objects.all()
+        to_price = request.query_params.get('to_price')
+        search= request.query_params.get('search')
+        ordering =request.query_params.get('ordering')
+        perpage=request.query_params.get('perpage',default=2)
+        page=request.query_params.get('page',default=1)
+        
+        if to_price:
+            items=items.filter(price__lte=to_price)
+        if search:
+            items=items.filter(title__contains=search)
+        if ordering:
+            ordering_fields= ordering.spilt(",")
+            items=items.order_by(*ordering_fields)
 
+        paginator=Paginator(items,per_page=perpage)
+        try:
+            items=paginator.page(number=page)
+        except EmptyPage:
+            items=[]
+        serialized_item = MenuItemSerializer(items, many=True)
+        return Response(serialized_item.data)
+    elif request.method in ['POST','PUT','DELETE','PATCH']:
+        if request.user.groups.filter(name='Manager').exists():
+           serialized_item =MenuItemSerializer(data=request.data)
+           serialized_item.is_valid(raise_exception=True)
+           serialized_item.save()
+           return Response(serialized_item.validated_data,status.HTTP_201_CREATED)
+        else :
+           return Response({"message":"youare not authorized "},403)
+        
 
-def display_menu_item(request, pk=None): 
-    if pk: 
-        menu_item = Menu.objects.get(pk=pk) 
-    else: 
-        menu_item = "" 
-    return render(request, 'menu_item.html', {"menu_item": menu_item}) 
-
+@api_view(['GET','PUT','DELETE','PATCH'])
+def single_item(request, pk):
+    if request.method=='GET':
+       item = get_object_or_404(Menu,id=pk)
+       serialized_item = MenuItemSerializer(item)
+       return Response(serialized_item.data)
+    
+    elif request.method == 'PUT':
+        if request.user.groups.filter(name='Manager').exists():
+           item=get_object_or_404( Menu ,id=pk)
+           serialized_item =MenuItemSerializer(item,data=request.data)
+           serialized_item.is_valid(raise_exception=True)
+           serialized_item.save()
+           return Response(serialized_item.validated_data,status.HTTP_201_CREATED)
+        else:
+            return Response({"message":"youare not authorized "},403)
+        
+    elif request.method == 'PATCH':
+        if request.user.groups.filter(name='Manager').exists():
+           item=get_object_or_404( Menu ,id=pk)
+           serialized_item =MenuItemSerializer(item,data=request.data,partial=True)
+           serialized_item.is_valid(raise_exception=True)
+           serialized_item.save()
+           return Response(serialized_item.validated_data,status.HTTP_201_CREATED)
+        else:
+            return Response({"message":"youare not authorized "},403)
+        
+    elif request.method =='DELETE':
+      if request.user.groups.filter(name='Manager').exists():
+        item=get_object_or_404( Menu ,id=pk)
+        item.delete()
+        return Response({"message":"ok "},status=status.HTTP_204_NO_CONTENT)
+      else:
+            return Response({"message":"youare not authorized "},status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        
+@api_view(['GET','POST','PUT','DELETE','PATCH'])
 @csrf_exempt
 def bookings(request) :
     if request.method == 'POST':
@@ -64,5 +125,5 @@ def bookings(request) :
 
     date = request.GET.get('date',datetime.today().date())
     bookings= Booking.objects.filter(reservation_date=date)
-    booking_json=serializers.serialize('json',bookings)
+    booking_json=BookingSerializer(bookings)
     return HttpResponse(booking_json,content_type='application/json')
